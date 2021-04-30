827bdaf8b62355f9dde44f2542b66d801a537336
Josh Long <josh@joshlong.com>
2021-04-30 11:12:02 -0700

    fail

--- src/main/java/com/example/failservice/FailServiceApplication.java
+++ src/main/java/com/example/failservice/FailServiceApplication.java
@@ -2,7 +2,18 @@ package com.example.failservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class FailServiceApplication {
 
@@ -10,4 +21,21 @@ public class FailServiceApplication {
 		SpringApplication.run(FailServiceApplication.class, args);
 	}
 
+	private final Object monitor = new Object();
+	private int counter = 0;
+
+	@PostMapping
+	ResponseEntity<?> acceptRetries(@RequestBody Map<String, String> payload) {
+
+		System.out.println(
+			"payload: " + payload + " , counter: " + this.counter + ", time: " + Instant.now());
+
+		synchronized (this.monitor) {
+			this.counter += 1;
+			if (this.counter < 5) {
+				return ResponseEntity.badRequest().build();
+			}
+		}
+		return ResponseEntity.ok().build();
+	}
 }
