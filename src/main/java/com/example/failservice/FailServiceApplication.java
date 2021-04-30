b0e1b336207040b3e56c03320c95bac11fe5934c
Josh Long <josh@joshlong.com>
2021-04-29 23:50:36 -0700

    fail

---
+++ src/main/java/com/example/failservice/FailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.failservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/failservice/FailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.failservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
