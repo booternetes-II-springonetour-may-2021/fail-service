ab8770a1863f6faf822d7462f72d78e5ea6233e4
Mark Heckler <mark.heckler@gmail.com>
2021-05-02 19:18:31 -0500

    Ready for failure

--- src/main/java/com/example/failservice/FailServiceApplication.java
+++ src/main/java/com/example/failservice/FailServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.failservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,48 +13,53 @@ import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@RestController
-@RequestMapping("/")
 @SpringBootApplication
 public class FailServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FailServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FailServiceApplication.class, args);
+    }
+}
 
-	private final Object monitor = new Object();
-	private int counter = 0;
+@RestController
+@RequestMapping("/")
+class FailController {
+    private final Object monitor = new Object();
+    private int counter = 0;
 
-	//	@PostMapping
-	ResponseEntity<?> timeout(@RequestBody Map<String, String> payload)
-		throws Exception {
+    @PostMapping("/ok")
+    ResponseEntity<?> ok(@RequestBody Map<String, String> payload) {
+        System.out.println("> Payload: " + payload
+                + ", time: " + Instant.now());
 
-		System.out.println(
-			"timeout payload: " + payload + ", time: " + Instant.now());
+        return ResponseEntity.ok().build();
+    }
 
-		Thread.sleep(8_000);
-		return ResponseEntity.ok().build();
-	}
+    @PostMapping("/timeout")
+    ResponseEntity<?> timeout(@RequestBody Map<String, String> payload)
+            throws Exception {
 
-	@PostMapping
-	ResponseEntity<?> ok(@RequestBody Map<String, String> payload) {
-		System.out.println(
-			"retry payload: " + payload + " , counter: " + this.counter + ", time: " + Instant.now());
-		return ResponseEntity.ok().build();
-	}
+        System.out.println(">>> Timeout payload: " + payload
+                + ", time: " + Instant.now());
 
-	@PostMapping
-	ResponseEntity<?> acceptRetries(@RequestBody Map<String, String> payload) {
-		System.out.println(
-			"retry payload: " + payload + " , counter: " + this.counter + ", time: " + Instant.now());
-		synchronized (this.monitor) {
-			this.counter += 1;
-			if (this.counter < 5) {
-				System.out.println("ERROR!");
-				return ResponseEntity.badRequest().build();
-			}
-		}
-		System.out.println("OK!");
-		return ResponseEntity.ok().build();
-	}
+        Thread.sleep(8_000);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/retry")
+    ResponseEntity<?> acceptRetries(@RequestBody Map<String, String> payload) {
+        System.out.println(">> Retry payload: " + payload
+                + " , counter: " + this.counter
+                + ", time: " + Instant.now());
+
+        synchronized (monitor) {
+            counter++;
+            if (counter < 5) {
+                System.out.println("<< Failure " + counter + " >>");
+                return ResponseEntity.badRequest().build();
+            }
+        }
+        System.out.println("<< Retry successful! >>");
+        return ResponseEntity.ok().build();
+    }
 }
